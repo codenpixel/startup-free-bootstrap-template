27a463bc64ff604e6d2666eb19322bb591987945
ShareBootstrap.com <almir.pamukovic@gmail.com>
2020-10-15 11:16:54 +0200

    Add files via upload

---
+++ src/js/signature.js
@@ -0,0 +1,14 @@
+dqSignature();
+
+function dqSignature() {
+
+    var signature = '';
+
+    signature += "\n" + '   Website designed by https://sharebootstrap.com/';
+
+
+    console.group('Its free ! Just give us some credit.');
+    console.log(signature);
+    console.groupEnd();
+
+}
